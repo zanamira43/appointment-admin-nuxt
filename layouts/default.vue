adc3cddebaebe107383be035cc2255fd93d1a9de
zanamira43 <zanamira43@gmail.com>
2025-02-19 14:25:44 +0300

    build components layout directories

---
+++ layouts/default.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="h-screen w--full">
+        <AppNavbar />
+        <div class="container mx-auto">
+            <slot />
+        </div>
+    </div>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,6 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   ssr: false,
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   modules: ['@nuxt/ui', '@nuxtjs/tailwindcss', '@pinia/nuxt']
 })
