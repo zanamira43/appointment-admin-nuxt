8ccaaa08fd947e0ac23a571a86758c7449845644
zanamira43 <zanamira43@gmail.com>
2025-02-22 17:17:24 +0300

    create users page

---
+++ pages/admin/users/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+const admin = ref("admin")
+
+</script>
+<template>
+ <NuxtLayout name="admin">
+	<div class="w-full h-auto">
+		<div class="px-4 py-2">
+			<DashboardPageHeader title="Users" subtitle="List of all users" />
+		</div>
+	</div>
+ </NuxtLayout>
+</template>
