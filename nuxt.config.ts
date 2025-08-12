863b413729141083dda19a267ce775bd6400b7fc
zanamira43 <zanamira43@gmail.com>
2025-08-12 13:18:11 +0300

    chagne some configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,3 +1,5 @@
+import fs from 'fs'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
@@ -12,11 +14,20 @@ export default defineNuxtConfig({
   
   runtimeConfig: {  
     public: {  
-      APIURL: process.env.API_URL
+      APIURL: import.meta.env.VITE_API_BASE_URL
     }
   },
   plugins: ['~/plugins/vue-query.ts',  '~/plugins/app-startup.ts'],
 
   ssr: false,
 
+  // devServer: {
+  //   port: 3000,
+  //   https: {
+  //     key: fs.readFileSync('./localhost-key.pem', 'utf-8'),
+  //     cert: fs.readFileSync('./localhost.pem','utf-8'),
+  //   }
+  // }
+
+  
 })
