c0e920d28c43f95b1ba5f702ca639f57a7820d27
zanamira43 <zanamira43@gmail.com>
2026-03-28 10:55:26 +0300

    ref: change table head color

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,10 +1,13 @@
 export default defineAppConfig({
-  // ui: {
-  //   global: true,
-  //   notifications: {
-  //     position: 'top-0 bottom-[unset]'
-  //   }
-  // }
+  ui: {
+    table: {
+      slots: {
+        root: 'border rounded-lg',
+        thead: 'rounded-t-lg',
+        th: 'bg-gray-800 text-gray-100'
+      }
+    }
+  }
 })
 function defineAppConfig<T extends Record<string, any>>(config: T): T {
   return config;
